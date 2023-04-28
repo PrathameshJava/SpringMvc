baafa32e6cc563535e1191dc0e1241f70b8b561e
Prathamesh Acharya <pratheking123@gmail.com>
2023-04-28 10:28:24 -0400

    removed unuse code

--- SpringMVC_ProductService/src/main/java/com/java/repository/ProductDao.java
+++ SpringMVC_ProductService/src/main/java/com/java/repository/ProductDao.java
@@ -1,8 +1,5 @@
 package com.java.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- SpringMVC_ProductService/src/main/java/com/java/service/ProductServices.java
+++ SpringMVC_ProductService/src/main/java/com/java/service/ProductServices.java
@@ -1,11 +1,6 @@
 package com.java.service;
 
-import java.io.IOException;
 import java.util.List;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.java.entity.Product;
 import com.java.exception.ResourceNotFoundException;
